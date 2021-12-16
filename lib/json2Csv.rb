10550afd9539e84c6ca7bc305c16d51da00eb767
Isabelle Lafont <isalafont@gmail.com>
2021-12-16 11:04:38 +0100

    Initialize Lib
    Create a lib directory
    create a json2Csv file
    Create spec directory

---
+++ lib/json2Csv.rb
@@ -0,0 +1,14 @@
+require 'csv'
+require 'json'
+
+Class Json2Csv
+
+  #  Create method
+  # 1 - Open csv new file to write in it.
+  # 2- Read JSON and parse it. There is Hash with Keys and Values and nested Keys and values
+  # => Keys = header into CSV
+  # => Values = row under each Header
+  # 3 -> Save CSV
+
+end
+
